ebf7f166f81c579ac6f5bdb96db0909826f8fac4
satyam <satyamkale271@gmail.com>
2025-02-09 16:34:23 +0530

    store setup

--- lib/store/store.ts
+++ lib/store/store.ts
@@ -1,1 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { videoReducer } from "../slices/videoUrlSlice";
+
+export const store = configureStore({
+  reducer: {
+    url: videoReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
